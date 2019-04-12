39c481031545058fa35c0118af4c156cee06bfeb
Tyler McCabe <mccabety@gmail.com>
2019-04-11 22:41:37 -0700

    Added basic MVVM structure

--- app/src/main/java/com/example/electronicstoolkit/views/MainActivity.java
+++ app/src/main/java/com/example/electronicstoolkit/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.electronicstoolkit;
+package com.example.electronicstoolkit.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.electronicstoolkit.R;
 
 public class MainActivity extends AppCompatActivity {
 
